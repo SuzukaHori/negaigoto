671d1b6a3161e35154c379951b10df6a14250fd2
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-08-06 16:58:53 +0900

    削除・詳細ページを作成

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -3,11 +3,30 @@ class WishesController < ApplicationController
     @wishes = Wish.all
   end
 
-#   def show
-#     @wish = Wish.find(params[:id])
-#   end
+  def show
+    puts "ああああああああああ"
+    @wish = Wish.find(params[:id])
+  end
 
-#   def create
-#     @wish = Wish.new()
-#   end
+  def create
+    wish = Wish.new(wish_params)
+    wish.save
+    redirect_to wishes_path
+  end
+
+  def new
+    @wish = Wish.new
+  end
+
+  def destroy
+    puts "-----------------------------------"
+    wish = Wish.find(params[:id])
+    wish.destroy
+    redirect_to wishes_path
+  end
+
+  private
+  def wish_params
+    params.require(:wish).permit(:context, :writer)
+  end
 end
