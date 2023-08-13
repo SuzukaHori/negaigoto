87bfaa1f523809f21261d87f17175321508a019b
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-08-13 16:41:56 +0900

    rspecをインストール

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -1,4 +1,4 @@
 class Wish < ApplicationRecord
-  validates :writer,  presence: true
-  validates :context, presence: true
+  validates :writer,  presence: true, length: { maximum: 25 }
+  validates :context, presence: true, length: { maximum: 100 }
 end
